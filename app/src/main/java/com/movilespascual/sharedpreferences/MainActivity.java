8f99ffc04ca359fe01ba0a524f3c00b9fd4c321f
MAGA1314 <garciaagudelomiguelangel@gmail.com>
2024-03-04 17:02:36 -0500

    ss

--- app/src/main/java/com/movilespascual/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/movilespascual/sharedpreferences/MainActivity.java
@@ -32,5 +32,7 @@ public class MainActivity extends AppCompatActivity {
         objetoEditor.putStringSet("usuarios", usuario.getText().toString());
         objetoEditor.commit();
 
+
+        
     }
 }
